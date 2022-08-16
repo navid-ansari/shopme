bbdab3b8f468b38680c56cb648c40057d715cc43
Navid <navidansari25july@gmail.com>
2022-08-16 17:59:00 +0530

    footer added and tablet responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,40 @@
 import React from "react";
 
 export const Footer = () => {
- return <div>Footer</div>;
+ return (
+  <div className="footer">
+   <div className="contact">
+    <h4>
+     <i class="ri-contacts-book-2-line"></i> Contact
+    </h4>
+    <p>
+     <i class="ri-smartphone-line"></i> 9028899889
+    </p>
+    <p>
+     <i class="ri-mail-line"></i> contact@shopme.com
+    </p>
+   </div>
+   <div className="support">
+    <h4>
+     <i class="ri-customer-service-2-line"></i> Customer Care
+    </h4>
+    <p>
+     <i class="ri-phone-line"></i> 1800 290 290
+    </p>
+    <p>
+     <i class="ri-mail-line"></i> support@shopme.com
+    </p>
+   </div>
+   <div className="copyright">
+    <h4>
+     <i class="ri-copyright-line"></i> Copyright
+    </h4>
+    <p>
+     <i class="ri-copyright-line"></i>2022 All rights reserved. 'SHOP ME' and
+     'SHOP ME' logo are trademark of Shop Me Inc.
+    </p>
+   </div>
+  </div>
+ );
 };
 export default Footer;
--- src/pages/Product-List.js
+++ src/pages/Product-List.js
@@ -1,60 +1,13 @@
-import React, { useEffect } from "react";
-
-// redux
-import { useSelector, useDispatch } from "react-redux";
-import { setProducts } from "../redux/actions/productAction";
-import { toggleFavoriteProduct } from "../redux/actions/favoriteProductsAction";
-import { cartAction } from "../redux/actions/cartAction";
-
-// axios
-import axios from "axios";
+import React from "react";
 
 // component
 import Product from "../components/Product";
 
+// custom hook
+import useProductActionHook from "../hooks/productActionHook";
+
 const ProductList = () => {
- const products = useSelector((state) => state.allProducts.products);
- const dispatch = useDispatch();
-
- const toggleFavorite = async (product) => {
-  const findProduct = products.find((item) => item.id === product.id);
-
-  await dispatch(toggleFavoriteProduct(findProduct));
-
-  if (findProduct) {
-   const modifiedProducts = products.map((product) => {
-    if (product.id === findProduct.id) {
-     return {
-      ...product,
-      isFavorite: !findProduct.isFavorite,
-     };
-    } else {
-     return product;
-    }
-   });
-   await dispatch(setProducts(modifiedProducts));
-  }
- };
-
- const toggleCart = async (product) => {
-  const findProduct = products.find((item) => item.id === product.id);
-
-  await dispatch(cartAction(findProduct));
-
-  if (findProduct) {
-   const modifiedProducts = products.map((product) => {
-    if (product.id === findProduct.id) {
-     return {
-      ...product,
-      isAddedToCart: !findProduct.isAddedToCart,
-     };
-    } else {
-     return product;
-    }
-   });
-   await dispatch(setProducts(modifiedProducts));
-  }
- };
+ const { products, toggleFavorite, toggleCart } = useProductActionHook();
 
  const productsElem = products.map((product) => (
   <Product
@@ -64,30 +17,6 @@ const ProductList = () => {
    toggleCart={() => toggleCart(product)}
   />
  ));
-
- useEffect(() => {
-  if (products.length === 0) {
-   const getProducts = async () => {
-    const url = "https://fakestoreapi.com/products";
-    const { data } = await axios.get(url).catch((error) => {
-     console.log(error);
-    });
-
-    // add 'isFavorite' & 'isAddedToCart' property
-    const modifiedProducts = data.map((product) => {
-     return {
-      ...product,
-      isAddedToCart: false,
-      isFavorite: false,
-     };
-    });
-
-    // dispatch data to store
-    dispatch(setProducts(modifiedProducts));
-   };
-   getProducts();
-  }
- }, []);
 
  return (
   <div className="productlist-page">
