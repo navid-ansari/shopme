73a30f1b22043cf1a7b01e34fce6aef50544ca15
Navid <navidansari25july@gmail.com>
2022-08-17 01:23:21 +0530

    Jest basic setup

---
+++ src/App.test.js
@@ -0,0 +1,5 @@
+describe("App component", () => {
+ test("Test app component", () => {
+  expect(2).toBe(2);
+ });
+});
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,33 +5,33 @@ export const Footer = () => {
   <div className="footer">
    <div className="contact">
     <h4>
-     <i class="ri-contacts-book-2-line"></i> Contact
+     <i className="ri-contacts-book-2-line"></i> Contact
     </h4>
     <p>
-     <i class="ri-smartphone-line"></i> 9028899889
+     <i className="ri-smartphone-line"></i> 9028899889
     </p>
     <p>
-     <i class="ri-mail-line"></i> contact@shopme.com
+     <i className="ri-mail-line"></i> contact@shopme.com
     </p>
    </div>
    <div className="support">
     <h4>
-     <i class="ri-customer-service-2-line"></i> Customer Care
+     <i className="ri-customer-service-2-line"></i> Customer Care
     </h4>
     <p>
-     <i class="ri-phone-line"></i> 1800 290 290
+     <i className="ri-phone-line"></i> 1800 290 290
     </p>
     <p>
-     <i class="ri-mail-line"></i> support@shopme.com
+     <i className="ri-mail-line"></i> support@shopme.com
     </p>
    </div>
    <div className="copyright">
     <h4>
-     <i class="ri-copyright-line"></i> Copyright
+     <i className="ri-copyright-line"></i> Copyright
     </h4>
     <p>
-     <i class="ri-copyright-line"></i>2022 All rights reserved. 'SHOP ME' and
-     'SHOP ME' logo are trademark of Shop Me Inc.
+     <i className="ri-copyright-line"></i>2022 All rights reserved. 'SHOP ME'
+     and 'SHOP ME' logo are trademark of Shop Me Inc.
     </p>
    </div>
   </div>
