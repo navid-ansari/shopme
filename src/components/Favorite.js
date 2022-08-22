d6694c4549a99995ec683ddabc534aba7f09c1ce
Navid <navidansari25july@gmail.com>
2022-08-22 15:34:22 +0530

    page css changed and detail component added

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,41 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 // component
-import Stars from "../components/Stars";
+import Detail from "./Detail";
 
 const Favorite = (props) => {
  const { category, description, id, image, price, rating, title } =
   props.favorite;
  const { rate, count } = rating || {};
 
- return (
-  <div className="product">
-   <div className="left-column">
-    <img src={image} alt="" />
-   </div>
-   <div className="right-column">
-    <h2 className="product-title">{title}</h2>
-    <div className="product-description">
-     <span className="decription">Description: </span>
-     <span className="description-text">{description}</span>
-    </div>
-    <div className="product-rating">
-     <span className="rating">Rating: </span>
-     <span className="rating-stars">
-      <Stars rating={rate} />
-     </span>
-    </div>
-    <div className="product-review">
-     <span className="reviews">Reviews: </span>
-     <span className="total-reviews">{count} reviews</span>
-    </div>
-    <div className="product-price">
-     <span className="price">Price: </span>
-     <span className="total-price">&#x20b9; {price}</span>
-    </div>
-   </div>
-  </div>
- );
+ return <Detail product={props.favorite} />;
 };
 
 export default Favorite;
--- src/pages/Product-Detail.js
+++ src/pages/Product-Detail.js
@@ -16,6 +16,9 @@ import {
 // component
 import Stars from "../components/Stars";
 
+// component
+import Detail from "../components/Detail";
+
 const ProductDetail = () => {
  const { productId } = useParams();
  const product = useSelector((state) => state.product);
@@ -43,35 +46,8 @@ const ProductDetail = () => {
  }, [productId]);
 
  return (
-  <div className="productdetail-page">
-   <div className="container">
-    <div className="product">
-     <div className="left-column">
-      <img src={image} alt="" />
-     </div>
-     <div className="right-column">
-      <h2 className="product-title">{title}</h2>
-      <div className="product-description">
-       <span className="decription">Description: </span>
-       <span className="description-text">{description}</span>
-      </div>
-      <div className="product-rating">
-       <span className="rating">Rating: </span>
-       <span className="rating-stars">
-        <Stars rating={rate} />
-       </span>
-      </div>
-      <div className="product-review">
-       <span className="reviews">Reviews: </span>
-       <span className="total-reviews">{count} reviews</span>
-      </div>
-      <div className="product-price">
-       <span className="price">Price: </span>
-       <span className="total-price">&#x20b9; {price}</span>
-      </div>
-     </div>
-    </div>
-   </div>
+  <div className="detail-page">
+   <Detail product={product} />
   </div>
  );
 };
